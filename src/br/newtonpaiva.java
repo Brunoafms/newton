b3d695be9006f81068287f7e0c3302f631b2db4f
12120717 <brunoafsm@gmail.com>
2023-02-24 20:26:23 -0300

    feat: cria estrutura inicial

---
+++ src/br/newtonpaiva.java
@@ -0,0 +1,28 @@
+package br;
+
+public class newtonpaiva
+{
+private Integer Numero;
+
+private Double saldo;
+
+public Double sacar(Double valor)
+{
+    return 0.0;
+}
+
+public Double depositar (Double valor){
+    return 0.0;
+}
+
+public Integer getNumero()
+{
+    return Numero;
+}
+
+public void setNumero(Integer Numero)
+{
+    Numero = Numero;
+}
+
+}
