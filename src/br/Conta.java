48b93bf8f0ba1ef23ebac62f9072c658bab8e84d
12120717 <brunoafsm@gmail.com>
2023-02-24 21:49:48 -0300

    feat: cria estrutura inicial

--- src/br/Conta.java
+++ src/br/Conta.java
@@ -2,18 +2,23 @@ package br;
 
 public class Conta {
 
-        public static void main(String[] args) {
+        public static void main(String[] args)
+        {
             new Conta();
             Conta c = new Conta();
             c.setNumero(10);
             c.setsaldo(100.0);
             c.depositar(50.0);
 
-            System.out.println(c.SetSaldo());
+            System.out.println(c.Setsaldo());
 
             public double depositar (Double valor){
-                saldo
-            }
+
+                if(valor == null || valor <=0)
+                    throw new IllegalAccessException()
+        }
+
+
         }
     }
 
