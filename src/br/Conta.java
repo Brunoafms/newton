6fbba66a6a21815d2e11a3800a9ed2ad02204dba
12120717 <brunoafsm@gmail.com>
2023-02-24 21:43:20 -0300

    feat: cria estrutura inicial

---
+++ src/br/Conta.java
@@ -0,0 +1,20 @@
+package br;
+
+public class Conta {
+
+        public static void main(String[] args) {
+            new Conta();
+            Conta c = new Conta();
+            c.setNumero(10);
+            c.setsaldo(100.0);
+            c.depositar(50.0);
+
+            System.out.println(c.SetSaldo());
+
+            public double depositar (Double valor){
+                saldo
+            }
+        }
+    }
+
+
--- src/br/newtonpaiva.java
+++ src/br/newtonpaiva.java
@@ -27,3 +27,4 @@ public void setNumero(Integer Numero)
 }
 
 }
+
